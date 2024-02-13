5b2b0f0695cddd76e596c9abc2d76b1159be7c61
Vladtsrez <Competitionvladinf@gmail.com>
2024-02-13 22:44:56 +0200

    Add

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 function loadApp() {
+  var flipbook = $('.flipbook');
 
-	var flipbook = $('.flipbook');
+  if (flipbook.width() == 0 || flipbook.height() == 0) {
+    setTimeout(loadApp, 10);
+    return;
+  }
 
-	if (flipbook.width()==0 || flipbook.height()==0) {
-		setTimeout(loadApp, 10);
-		return;
-	}
-
-	$('.flipbook .double').scissor();
-	$('.flipbook').turn({
-			elevation: 50,
-			gradients: true,
-			autoCenter: true
-	});
+  $('.flipbook .double').scissor();
+  $('.flipbook').turn({
+    elevation: 50,
+    gradients: true,
+    autoCenter: true,
+  });
 }
 
 yepnope({
-	test : Modernizr.csstransforms,
-	yep: ['lib/turn.min.js'],
-	nope: ['lib/turn.html4.min.js'],
-	both: ['lib/scissor.min.js', 'index.css'],
-	complete: loadApp
+  test: Modernizr.csstransforms,
+  yep: ['lib/turn.min.js'],
+  nope: ['lib/turn.html4.min.js'],
+  both: ['lib/scissor.min.js', 'index.css'],
+  complete: loadApp,
 });
